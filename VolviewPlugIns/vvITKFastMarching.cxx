b6f3e72b10ffd2ede05e9c376d8d370365c4a862
Luis Ibanez <luis.ibanez@kitware.com>
2003-03-29 15:41:35 -0500

    ENH: Converted to the new Plugin architecture. Communication is now done      with an API based on strings instead of getting access to structure members.

--- VolviewPlugIns/vvITKFastMarching.cxx
+++ VolviewPlugIns/vvITKFastMarching.cxx
@@ -13,6 +13,28 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
 
   const unsigned int Dimension = 3;
 
+  const float stoppingValue         = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ));
+  const float normalizationFactor   = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ));
+
+  const unsigned int numberOfSeeds = info->NumberOfMarkers;
+  if( numberOfSeeds < 1 )
+    {
+    info->SetProperty( info, VVP_ERROR, "Please select points using the 3D Markers in the Annotation menu" ); 
+    return -1;
+    }
+
+  // Take the first marker as the seed point
+  const float * seedCoordinates = info->Markers;
+
+  itk::Size<3> size;
+  size[0] = info->OutputVolumeDimensions[0];
+  size[1] = info->OutputVolumeDimensions[1];
+  size[2] = info->OutputVolumeDimensions[2];
+
+  const double seedValue = 0.0;
+
+  itk::Index<Dimension> seedPosition;
+
   try 
   {
   switch( info->InputVolumeScalarType )
@@ -26,29 +48,27 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
       typedef  FilterType::NodeType                 NodeType;
       NodeContainer::Pointer seeds = NodeContainer::New();
       seeds->Initialize();
-      const double seedValue = 0.0;
-      ImageType::IndexType seedPosition;
       VolView::PlugIn::FilterModule< FilterType > module;
       module.SetPluginInfo( info );
       module.SetUpdateMessage("Computing Fast Marching...");
       // Set the parameters on it
-      module.GetFilter()->SetStoppingValue(  atof( info->GUIItems[ 0 ].CurrentValue) );
-      seedPosition[0] =                      atoi( info->GUIItems[ 1 ].CurrentValue);      
-      seedPosition[1] =                      atoi( info->GUIItems[ 2 ].CurrentValue);      
-      seedPosition[2] =                      atoi( info->GUIItems[ 3 ].CurrentValue);      
-      module.GetFilter()->SetNormalizationFactor(  atof( info->GUIItems[ 4 ].CurrentValue) );
+      module.GetFilter()->SetStoppingValue(  stoppingValue );
+      module.GetFilter()->SetNormalizationFactor( normalizationFactor );
       NodeType node;
       node.SetValue( seedValue );
-      node.SetIndex( seedPosition );
-      seeds->InsertElement( 0, node );
+      for(unsigned int i=0; i< numberOfSeeds; i++)
+        {
+        seedPosition[0] =  static_cast< int >( seedCoordinates[0] );
+        seedPosition[1] =  static_cast< int >( seedCoordinates[1] );
+        seedPosition[2] =  static_cast< int >( seedCoordinates[2] );
+        node.SetIndex( seedPosition );
+        seeds->InsertElement( i, node );
+        seedCoordinates += 3; // pass to next point
+        }
       module.GetFilter()->SetTrialPoints( seeds );
-      ImageType::SizeType size;
-      size[0] = info->OutputVolumeDimensions[0];
-      size[1] = info->OutputVolumeDimensions[1];
-      size[2] = info->OutputVolumeDimensions[2];
       module.GetFilter()->SetOutputSize( size );
       // Execute the filter
-      module.ProcessData( pds  );
+      module.ProcessData( pds );
       break; 
       }
     case VTK_UNSIGNED_SHORT:
@@ -60,26 +80,24 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
       typedef  FilterType::NodeType                 NodeType;
       NodeContainer::Pointer seeds = NodeContainer::New();
       seeds->Initialize();
-      const double seedValue = 0.0;
-      ImageType::IndexType seedPosition;
       VolView::PlugIn::FilterModule< FilterType > module;
       module.SetPluginInfo( info );
       module.SetUpdateMessage("Computing Fast Marching...");
       // Set the parameters on it
-      module.GetFilter()->SetStoppingValue(  atof( info->GUIItems[ 0 ].CurrentValue) );
-      seedPosition[0] =                      atoi( info->GUIItems[ 1 ].CurrentValue);      
-      seedPosition[1] =                      atoi( info->GUIItems[ 2 ].CurrentValue);      
-      seedPosition[2] =                      atoi( info->GUIItems[ 3 ].CurrentValue);      
-      module.GetFilter()->SetNormalizationFactor(  atof( info->GUIItems[ 4 ].CurrentValue) );
+      module.GetFilter()->SetStoppingValue(  stoppingValue );
+      module.GetFilter()->SetNormalizationFactor( normalizationFactor );
       NodeType node;
       node.SetValue( seedValue );
-      node.SetIndex( seedPosition );
-      seeds->InsertElement( 0, node );
+      for(unsigned int i=0; i< numberOfSeeds; i++)
+        {
+        seedPosition[0] =  static_cast< int >( seedCoordinates[0] );
+        seedPosition[1] =  static_cast< int >( seedCoordinates[1] );
+        seedPosition[2] =  static_cast< int >( seedCoordinates[2] );
+        node.SetIndex( seedPosition );
+        seeds->InsertElement( i, node );
+        seedCoordinates += 3; // pass to next point
+        }
       module.GetFilter()->SetTrialPoints( seeds );
-      ImageType::SizeType size;
-      size[0] = info->OutputVolumeDimensions[0];
-      size[1] = info->OutputVolumeDimensions[1];
-      size[2] = info->OutputVolumeDimensions[2];
       module.GetFilter()->SetOutputSize( size );
       // Execute the filter
       module.ProcessData( pds );
@@ -89,7 +107,7 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
   }
   catch( itk::ExceptionObject & except )
   {
-    info->DisplayError( info, except.what() ); 
+    info->SetProperty( info, VVP_ERROR, except.what() ); 
     return -1;
   }
   return 0;
@@ -100,43 +118,23 @@ static int UpdateGUI(void *inf)
 {
   vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
 
-  info->GUIItems[0].Label = "Stopping Value";
-  info->GUIItems[0].GUIType = VV_GUI_SCALE;
-  info->GUIItems[0].Default = "1.0";
-  info->GUIItems[0].Help = "Defines a stopping value for the time up to which the front propagation will be computed."; 
-  info->GUIItems[0].Hints = "1 100 1";
+  info->SetGUIProperty(info, 0, VVP_GUI_LABEL, "Stopping Value");
+  info->SetGUIProperty(info, 0, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 0, VVP_GUI_DEFAULT, "50.0");
+  info->SetGUIProperty(info, 0, VVP_GUI_HELP, "Defines a stopping value for the time up to which the front propagation will be computed.");
+  info->SetGUIProperty(info, 0, VVP_GUI_HINTS , "1 100 1");
 
-  info->GUIItems[1].Label = "X coordinate of the seed";
-  info->GUIItems[1].GUIType = VV_GUI_SCALE;
-  info->GUIItems[1].Default = "0";
-  info->GUIItems[1].Help = "X coordinate of the seed point. The seed should be placed in the middle of the region to be segmented.";
-  info->GUIItems[1].Hints = "1 1000.0 1.0";
+  info->SetGUIProperty(info, 1, VVP_GUI_LABEL, "Speed image normalization factor");
+  info->SetGUIProperty(info, 1, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 1, VVP_GUI_DEFAULT, "1.0");
+  info->SetGUIProperty(info, 1, VVP_GUI_HELP, "Factor to be used for dividing the pixel values of the speed image. This allows to use images of integer pixel type for representing the speed. The normalization should map the values of the integer image into the range [0,1]");
+  info->SetGUIProperty(info, 1, VVP_GUI_HINTS , "1.0  1000.0  1.0");
 
-  info->GUIItems[2].Label = "Y coordinate of the seed";
-  info->GUIItems[2].GUIType = VV_GUI_SCALE;
-  info->GUIItems[2].Default = "0";
-  info->GUIItems[2].Help = "Y coordinate of the seed point. The seed should be placed in the middle of the region to be segmented.";
-  info->GUIItems[2].Hints = "1 1000.0 1.0";
-
-  info->GUIItems[3].Label = "Z coordinate of the seed";
-  info->GUIItems[3].GUIType = VV_GUI_SCALE;
-  info->GUIItems[3].Default = "0";
-  info->GUIItems[3].Help = "Z coordinate of the seed point. The seed should be placed in the middle of the region to be segmented.";
-  info->GUIItems[3].Hints = "1 1000.0 1.0";
-
-  info->GUIItems[4].Label = "Speed image normalization factor";
-  info->GUIItems[4].GUIType = VV_GUI_SCALE;
-  info->GUIItems[4].Default = "1";
-  info->GUIItems[4].Help = "Factor to be used for dividing the pixel values of the speed image. This allows to use images of integer pixel type for representing the speed. The normalization should map the values of the integer image into the range [0,1]";
-  info->GUIItems[4].Hints = "1 1000.0 1.0";
-
-
-
-  info->RequiredZOverlap = 0;
+  info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP, "0");
   
-  info->OutputVolumeScalarType = info->InputVolumeScalarType;
-  info->OutputVolumeNumberOfComponents = 
-    info->InputVolumeNumberOfComponents;
+  info->OutputVolumeScalarType = VTK_UNSIGNED_CHAR;
+  info->OutputVolumeNumberOfComponents = 1;
+
   memcpy(info->OutputVolumeDimensions,info->InputVolumeDimensions,
          3*sizeof(int));
   memcpy(info->OutputVolumeSpacing,info->InputVolumeSpacing,
@@ -155,20 +153,18 @@ void VV_PLUGIN_EXPORT vvITKFastMarchingInit(vtkVVPluginInfo *info)
   // setup information that never changes
   info->ProcessData = ProcessData;
   info->UpdateGUI = UpdateGUI;
-  info->Name = "Fast Marching (ITK)";
-  info->TerseDocumentation = "Fast Marching Level Set";
-  info->FullDocumentation = 
-    "Fast Marcing uses a Level Set representation for propagating a front from user-provided seed points. A user-provided speed image is used for controling the front propagation";
-  info->SupportsInPlaceProcessing = 0;
-  info->SupportsProcessingPieces = 0;
-  info->RequiredZOverlap = 0;
+  info->SetProperty(info, VVP_NAME, "Fast Marching (ITK)");
+  info->SetProperty(info, VVP_TERSE_DOCUMENTATION,
+                                    "Fast Marching Level Set");
+  info->SetProperty(info, VVP_FULL_DOCUMENTATION,
+    "Fast Marcing uses a Level Set representation for propagating a front from user-provided seed points. A user-provided speed image is used for controling the front propagation");
 
-  // Number of bytes required in intermediate memory per voxel
-  info->PerVoxelMemoryRequired = 16; 
-  
-  /* setup the GUI components */
-  info->NumberOfGUIItems = 5;
-  info->GUIItems = (vtkVVGUIItem *)malloc(info->NumberOfGUIItems*sizeof(vtkVVGUIItem));
+  info->SetProperty(info, VVP_SUPPORTS_IN_PLACE_PROCESSING, "0");
+  info->SetProperty(info, VVP_SUPPORTS_PROCESSING_PIECES,   "0");
+  info->SetProperty(info, VVP_NUMBER_OF_GUI_ITEMS,          "2");
+  info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP,           "0");
+  info->SetProperty(info, VVP_PER_VOXEL_MEMORY_REQUIRED,    "16");
+
 }
 
 }
