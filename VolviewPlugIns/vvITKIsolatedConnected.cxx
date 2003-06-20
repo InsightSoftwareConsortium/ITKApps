3547a677673a3669917883306091b06dd646de0f
Luis Ibanez <luis.ibanez@kitware.com>
2003-06-20 12:54:44 -0400

    ENH: Modified to produce single/double components output.      Template helper class added for supporting all input pixel types.

--- VolviewPlugIns/vvITKIsolatedConnected.cxx
+++ VolviewPlugIns/vvITKIsolatedConnected.cxx
@@ -1,48 +1,43 @@
-/* perform segmentation using the confidence connected image filter */
+/* perform segmentation using the Isolated Connected image filter */
 
-#include "vvITKFilterModule.h"
+#include "vvITKFilterModuleDoubleOutput.h"
 
 #include "itkIsolatedConnectedImageFilter.h"
 
-static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
-{
 
-  vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
 
-  const unsigned int Dimension = 3;
-
-  const float lower           = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ) );
-  const float upperLimit      = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ) );
-  const float upperTolerance  = atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ) );
-  const int   replaceValue    = atoi( info->GetGUIProperty(info, 3, VVP_GUI_VALUE ) );
-
-   if( info->NumberOfMarkers < 1 )
-    {
-    info->SetProperty( info, VVP_ERROR, "Please select a seed point using the 3D Markers in the Annotation menu" ); 
-    return -1;
-    }
-
-  itk::Index<Dimension> seed1;
-  itk::Index<Dimension> seed2;
-  VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, 0, seed1 );
-  VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, 1, seed2 );
-
-  char tmp[1024];
-
-  try 
+template <class InputPixelType>
+class IsolatedConnectedRunner
   {
-  switch( info->InputVolumeScalarType )
+  public:
+    itkStaticConstMacro( Dimension, unsigned int, 3);
+    typedef  itk::Image< InputPixelType, Dimension >  InputImageType; 
+    typedef  unsigned char                            OutputPixelType;
+    typedef  itk::Image< OutputPixelType, Dimension > OutputImageType; 
+    typedef  InputImageType::IndexType                IndexType;
+    typedef  itk::IsolatedConnectedImageFilter< InputImageType,  
+                                                OutputImageType >   FilterType;
+  public:
+    IsolatedConnectedRunner( vtkVVPluginInfo *info, vtkVVProcessDataStruct *pds )
     {
-    case VTK_UNSIGNED_CHAR:
-      {
-      typedef  unsigned char                             InputPixelType;
-      typedef  itk::Image< InputPixelType, Dimension >   InputImageType; 
-      typedef  unsigned char                             OutputPixelType;
-      typedef  itk::Image< OutputPixelType, Dimension >  OutputImageType; 
-      typedef  InputImageType::IndexType                 IndexType;
-      typedef  itk::IsolatedConnectedImageFilter< InputImageType,  
-                                                    OutputImageType >   FilterType;
-      VolView::PlugIn::FilterModule< FilterType > module;
+      const float lower           = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ) );
+      const float upperLimit      = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ) );
+      const float upperTolerance  = atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ) );
+      const int   replaceValue    = atoi( info->GetGUIProperty(info, 3, VVP_GUI_VALUE ) );
+      const bool  compositeOutput = atoi( info->GetGUIProperty(info, 4, VVP_GUI_VALUE ) );
+
+      if( info->NumberOfMarkers < 2 )
+        {
+        info->SetProperty( info, VVP_ERROR, "Please select two seed point using the 3D Markers in the Annotation menu. The first seed point in the region to segment, the second seed point in the region to reject." ); 
+        return;
+        }
+
+      itk::Index<Dimension> seed1;
+      itk::Index<Dimension> seed2;
+      VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, 0, seed1 );
+      VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, 1, seed2 );
+
+      VolView::PlugIn::FilterModuleDoubleOutput< FilterType > module;
       module.SetPluginInfo( info );
       module.SetUpdateMessage("Isolated Connected Region Growing...");
       // Set the parameters on it
@@ -55,45 +50,84 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
       // Execute the filter
       module.ProcessData( pds  );
       const float isolatedValue = static_cast<float>( module.GetFilter()->GetIsolatedValue() );
+      char tmp[1024];
       sprintf( tmp, "Upper threshold found = %f\n This is the intensity value that disconnects the two seeds",
                isolatedValue ); 
       info->SetProperty( info, VVP_REPORT_TEXT, tmp );
-      break; 
-      }
-    case VTK_UNSIGNED_SHORT:
+    }
+  };
+
+
+
+
+
+
+static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
+{
+
+  vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
+
+  try 
+    {
+    switch( info->InputVolumeScalarType )
       {
-      typedef  unsigned short                            InputPixelType;
-      typedef  itk::Image< InputPixelType, Dimension >   InputImageType; 
-      typedef  unsigned char                             OutputPixelType;
-      typedef  itk::Image< OutputPixelType, Dimension >  OutputImageType; 
-      typedef  InputImageType::IndexType                 IndexType;
-      typedef  itk::IsolatedConnectedImageFilter<  InputImageType,  
-                                                    OutputImageType >   FilterType;
-      VolView::PlugIn::FilterModule< FilterType > module;
-      module.SetPluginInfo( info );
-      module.SetUpdateMessage("Isolated Connected Region Growing...");
-      // Set the parameters on it
-      module.GetFilter()->SetIsolatedValueTolerance( static_cast<InputPixelType>( upperTolerance  ) );
-      module.GetFilter()->SetUpperValueLimit( static_cast<InputPixelType>( upperLimit  ) );
-      module.GetFilter()->SetLower( static_cast<InputPixelType>( lower ) );
-      module.GetFilter()->SetReplaceValue( replaceValue );
-      module.GetFilter()->SetSeed1( seed1 );
-      module.GetFilter()->SetSeed2( seed2 );
-      // Execute the filter
-      module.ProcessData( pds );
-      const float isolatedValue = static_cast<float>( module.GetFilter()->GetIsolatedValue() );
-      sprintf( tmp, "Upper threshold found = %f\n This is the intensity value that disconnects the two seeds",
-               isolatedValue ); 
-      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
-      break; 
+      case VTK_CHAR:
+        {
+        IsolatedConnectedRunner<char> runner( info, pds );
+        break; 
+        }
+      case VTK_UNSIGNED_CHAR:
+        {
+        IsolatedConnectedRunner<unsigned char> runner( info, pds );
+        break; 
+        }
+      case VTK_SHORT:
+        {
+        IsolatedConnectedRunner<signed short> runner( info, pds );
+        break; 
+        }
+      case VTK_UNSIGNED_SHORT:
+        {
+        IsolatedConnectedRunner<unsigned short> runner( info, pds );
+        break; 
+        }
+      case VTK_INT:
+        {
+        IsolatedConnectedRunner<signed int> runner( info, pds );
+        break; 
+        }
+      case VTK_UNSIGNED_INT:
+        {
+        IsolatedConnectedRunner<unsigned int> runner( info, pds );
+        break; 
+        }
+      case VTK_LONG:
+        {
+        IsolatedConnectedRunner<signed long> runner( info, pds );
+        break; 
+        }
+      case VTK_UNSIGNED_LONG:
+        {
+        IsolatedConnectedRunner<unsigned long> runner( info, pds );
+        break; 
+        }
+      case VTK_FLOAT:
+        {
+        IsolatedConnectedRunner<float> runner( info, pds );
+        break; 
+        }
+      case VTK_DOUBLE:
+        {
+        IsolatedConnectedRunner<double> runner( info, pds );
+        break; 
+        }
       }
     }
-  }
   catch( itk::ExceptionObject & except )
-  {
+    {
     info->SetProperty( info, VVP_ERROR, except.what() ); 
     return -1;
-  }
+    }
   return 0;
 }
 
@@ -126,11 +160,37 @@ static int UpdateGUI(void *inf)
   info->SetGUIProperty(info, 3, VVP_GUI_HELP, "Value to assign to the binary mask of the segmented region. The rest of the image will be set to zero.");
   info->SetGUIProperty(info, 3, VVP_GUI_HINTS , "1 255.0 1.0");
 
+  info->SetGUIProperty(info, 4, VVP_GUI_LABEL, "Produce composite output");
+  info->SetGUIProperty(info, 4, VVP_GUI_TYPE, VVP_GUI_CHECKBOX);
+  info->SetGUIProperty(info, 4, VVP_GUI_DEFAULT, "0");
+  info->SetGUIProperty(info, 4, VVP_GUI_HELP, "This filter produce by default a binary image as output. Enabling this option will instead generate a composite output combining the input image and the binary mask as an image of two components. This is convenient for evaluating the quality of a segmentation.");
+
   info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP, "0");
-  
- 
+
+  // Depending on this option, the filter will produce a 
+  // single component or a double component output.
+  // When single component is used, the output type is 'unsigned char'
+
   info->OutputVolumeScalarType = VTK_UNSIGNED_CHAR;
   info->OutputVolumeNumberOfComponents = 1;
+  info->SetProperty(info, VVP_PER_VOXEL_MEMORY_REQUIRED,    "4");
+
+  const char * compositeOutputProperty = info->GetGUIProperty(info, 4, VVP_GUI_VALUE ); 
+
+  // During the startup of the application this string is not yet defined.
+  // We should then check for it before trying to use it.
+  if( compositeOutputProperty )
+    {
+    const bool compositeOutput  = atoi( compositeOutputProperty );
+    if( compositeOutput )
+      {
+      info->OutputVolumeScalarType = info->InputVolumeScalarType;
+      info->OutputVolumeNumberOfComponents = 2;
+      info->SetProperty(info, VVP_PER_VOXEL_MEMORY_REQUIRED,    "8");
+      }
+    }
+
+
   memcpy(info->OutputVolumeDimensions,info->InputVolumeDimensions,
          3*sizeof(int));
   memcpy(info->OutputVolumeSpacing,info->InputVolumeSpacing,
@@ -140,6 +200,8 @@ static int UpdateGUI(void *inf)
 
   return 1;
 }
+
+
 
 extern "C" {
   
@@ -158,7 +220,7 @@ void VV_PLUGIN_EXPORT vvITKIsolatedConnectedInit(vtkVVPluginInfo *info)
     "This filter applies a region growing algorithm for segmentation. You must provide two seed points, the filter will extract a region connected to the first seed point and not connected to the second one. This is quite useful for separating adjacent structures. The method will grow a region around the first seed point in such a way that the pixels in the region have intensities higher than the lower threshold. The upper threshold is computed by the filter as the value that prevents the seed2 point to be included in the region of seed1.");
   info->SetProperty(info, VVP_SUPPORTS_IN_PLACE_PROCESSING, "0");
   info->SetProperty(info, VVP_SUPPORTS_PROCESSING_PIECES,   "0");
-  info->SetProperty(info, VVP_NUMBER_OF_GUI_ITEMS,          "4");
+  info->SetProperty(info, VVP_NUMBER_OF_GUI_ITEMS,          "5");
   info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP,           "0");
   info->SetProperty(info, VVP_PER_VOXEL_MEMORY_REQUIRED,    "4");// It is actually dependent of the complexity of the shape to segment
 
