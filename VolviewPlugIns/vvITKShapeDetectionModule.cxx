8d9cd12dbbbf3948308658f22b01f10f8e78a236
Luis Ibanez <luis.ibanez@kitware.com>
2003-08-27 16:48:25 -0400

    FIX: Now managing multiple pixel types correctly.

--- VolviewPlugIns/vvITKShapeDetectionModule.cxx
+++ VolviewPlugIns/vvITKShapeDetectionModule.cxx
@@ -1,9 +1,71 @@
 /* This module encapsulates the full preprocessing required for
-   applying the FastMarching image filter for segmenting the 
+   applying the ShapeDetection image filter for segmenting the 
    volume.  It requires seed points and the original image as inputs. */
 
 
 #include "vvITKShapeDetectionModule.txx"
+
+
+
+template <class InputPixelType>
+class ShapeDetectionModuleRunner
+  {
+  public:
+
+      typedef VolView::PlugIn::ShapeDetectionModule< 
+                                            InputPixelType >   ModuleType;
+
+  public:
+    ShapeDetectionModuleRunner() {}
+    void Execute( vtkVVPluginInfo *info, vtkVVProcessDataStruct *pds )
+    {
+
+      const float distanceFromSeeds     = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ));
+      const float gaussianSigma         = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ));
+      const float lowestBasinValue      = atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ));
+      const float lowestBorderValue     = atof( info->GetGUIProperty(info, 3, VVP_GUI_VALUE ));
+      const float curvatureScaling      = atof( info->GetGUIProperty(info, 4, VVP_GUI_VALUE ));
+      const float propagationScaling    = atof( info->GetGUIProperty(info, 5, VVP_GUI_VALUE ));
+      const float maximumRMSError       = atof( info->GetGUIProperty(info, 6, VVP_GUI_VALUE ));
+      const unsigned int maximumNumberOfIterations = atoi( info->GetGUIProperty(info, 7, VVP_GUI_VALUE ));
+
+
+      const unsigned int numberOfSeeds = info->NumberOfMarkers;
+      if( numberOfSeeds < 1 )
+        {
+        info->SetProperty( info, VVP_ERROR, "Please select points using the 3D Markers in the Annotation menu" ); 
+        return;
+        }
+
+      itk::Index<3> seedPosition;
+
+      ModuleType  module;
+      module.SetPluginInfo( info );
+      module.SetUpdateMessage("Computing Shape Detection Module...");
+      module.SetDistanceFromSeeds( distanceFromSeeds );
+      module.SetSigma( gaussianSigma );
+      module.SetLowestBasinValue( lowestBasinValue ); 
+      module.SetLowestBorderValue( lowestBorderValue );
+      module.SetCurvatureScaling( curvatureScaling );
+      module.SetPropagationScaling( propagationScaling );
+      module.SetMaximumRMSError( maximumRMSError );
+      module.SetMaximumIterations( maximumNumberOfIterations );
+      for(unsigned int i=0; i< numberOfSeeds; i++)
+        {
+        VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, i, seedPosition );
+        module.AddSeed( seedPosition );
+        }
+      // Execute the filter
+      module.ProcessData( pds  );
+
+      char tmp[1024];
+      sprintf(tmp,"Total number of iterations = %d \n Final RMS error = %f",
+                                                         module.GetElapsedIterations(),
+                                                         module.GetRMSChange());
+      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
+
+    }
+  };
 
 
 static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
@@ -11,90 +73,74 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
 
   vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
 
-  const float distanceFromSeeds     = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ));
-  const float gaussianSigma         = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ));
-  const float lowestBasinValue      = atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ));
-  const float lowestBorderValue     = atof( info->GetGUIProperty(info, 3, VVP_GUI_VALUE ));
-  const float curvatureScaling      = atof( info->GetGUIProperty(info, 4, VVP_GUI_VALUE ));
-  const float propagationScaling    = atof( info->GetGUIProperty(info, 5, VVP_GUI_VALUE ));
-  const float maximumRMSError       = atof( info->GetGUIProperty(info, 6, VVP_GUI_VALUE ));
-
-  const unsigned int maximumNumberOfIterations = atoi( info->GetGUIProperty(info, 7, VVP_GUI_VALUE ));
-
-  const unsigned int numberOfSeeds = info->NumberOfMarkers;
-  if( numberOfSeeds < 1 )
-    {
-    info->SetProperty( info, VVP_ERROR, "Please select points using the 3D Markers in the Annotation menu" ); 
-    return -1;
-    }
-
-  char tmp[1024];
-
-  itk::Index<3> seedPosition;
 
   try 
   {
   switch( info->InputVolumeScalarType )
     {
+    case VTK_CHAR:
+      {
+      ShapeDetectionModuleRunner<signed char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
     case VTK_UNSIGNED_CHAR:
       {
-      typedef  unsigned char                              PixelType;
-      typedef VolView::PlugIn::ShapeDetectionModule< 
-                                            PixelType >   ModuleType;
-      ModuleType  module;
-      module.SetPluginInfo( info );
-      module.SetUpdateMessage("Computing Shape Detection Module...");
-      module.SetDistanceFromSeeds( distanceFromSeeds );
-      module.SetSigma( gaussianSigma );
-      module.SetLowestBasinValue( lowestBasinValue ); 
-      module.SetLowestBorderValue( lowestBorderValue );
-      module.SetCurvatureScaling( curvatureScaling );
-      module.SetPropagationScaling( propagationScaling );
-      module.SetMaximumRMSError( maximumRMSError );
-      module.SetMaximumIterations( maximumNumberOfIterations );
-      for(unsigned int i=0; i< numberOfSeeds; i++)
-        {
-        VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, i, seedPosition );
-        module.AddSeed( seedPosition );
-        }
-      // Execute the filter
-      module.ProcessData( pds  );
-      sprintf(tmp,"Total number of iterations = %d \n Final RMS error = %f",
-                         module.GetElapsedIterations(),
-                         module.GetRMSChange());
-      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
+      ShapeDetectionModuleRunner<unsigned char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_SHORT:
+      {
+      ShapeDetectionModuleRunner<signed short> runner;
+      runner.Execute( info, pds );
       break; 
       }
     case VTK_UNSIGNED_SHORT:
       {
-      typedef  unsigned short                             PixelType;
-      typedef VolView::PlugIn::ShapeDetectionModule< 
-                                            PixelType >   ModuleType;
-      ModuleType  module;
-      module.SetPluginInfo( info );
-      module.SetDistanceFromSeeds( distanceFromSeeds );
-      module.SetSigma( gaussianSigma );
-      module.SetUpdateMessage("Computing Shape Detection Module...");
-      module.SetLowestBasinValue( lowestBasinValue ); 
-      module.SetLowestBorderValue( lowestBorderValue );
-      module.SetCurvatureScaling( curvatureScaling );
-      module.SetPropagationScaling( propagationScaling );
-      module.SetMaximumRMSError( maximumRMSError );
-      module.SetMaximumIterations( maximumNumberOfIterations );
-      for(unsigned int i=0; i< numberOfSeeds; i++)
-        {
-        VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, i, seedPosition );
-        module.AddSeed( seedPosition );
-        }
-      // Execute the filter
-      module.ProcessData( pds  );
-      sprintf(tmp,"Total number of iterations = %d \n Final RMS error = %f",
-                         module.GetElapsedIterations(),
-                         module.GetRMSChange());
-      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
+      ShapeDetectionModuleRunner<unsigned short> runner;
+      runner.Execute( info, pds );
       break; 
-      } 
+      }
+    case VTK_INT:
+      {
+      ShapeDetectionModuleRunner<signed int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_INT:
+      {
+      ShapeDetectionModuleRunner<unsigned int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_LONG:
+      {
+      ShapeDetectionModuleRunner<signed long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_LONG:
+      {
+      ShapeDetectionModuleRunner<unsigned long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_FLOAT:
+      {
+      ShapeDetectionModuleRunner<float> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_DOUBLE:
+      {
+      ShapeDetectionModuleRunner<double> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
     }
+
+
   }
   catch( itk::ExceptionObject & except )
   {
