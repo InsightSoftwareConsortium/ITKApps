ee4caa9d162209a181ee141dad711eb8042f0b32
Luis Ibanez <luis.ibanez@kitware.com>
2003-08-27 19:24:35 -0400

    FIX: now managing multiple data types correctly and checking for single      component dataset at input.

--- VolviewPlugIns/vvITKGeodesicActiveContourModule.cxx
+++ VolviewPlugIns/vvITKGeodesicActiveContourModule.cxx
@@ -6,98 +6,143 @@
 #include "vvITKGeodesicActiveContourModule.txx"
 
 
+
+template <class InputPixelType>
+class GeodesicActiveContourModuleRunner
+  {
+  public:
+    typedef VolView::PlugIn::GeodesicActiveContourModule< InputPixelType >   ModuleType;
+    
+  public:
+    GeodesicActiveContourModuleRunner() {}
+    void Execute( vtkVVPluginInfo *info, vtkVVProcessDataStruct *pds )
+    {
+      const float distanceFromSeeds     = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ));
+      const float gaussianSigma         = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ));
+      const float lowestBasinValue      = atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ));
+      const float lowestBorderValue     = atof( info->GetGUIProperty(info, 3, VVP_GUI_VALUE ));
+      const float curvatureScaling      = atof( info->GetGUIProperty(info, 4, VVP_GUI_VALUE ));
+      const float propagationScaling    = atof( info->GetGUIProperty(info, 5, VVP_GUI_VALUE ));
+      const float advectionScaling      = atof( info->GetGUIProperty(info, 6, VVP_GUI_VALUE ));
+      const float maximumRMSError       = atof( info->GetGUIProperty(info, 7, VVP_GUI_VALUE ));
+
+      const unsigned int maximumNumberOfIterations = atoi( info->GetGUIProperty(info, 8, VVP_GUI_VALUE ));
+
+      const unsigned int numberOfSeeds = info->NumberOfMarkers;
+
+      char tmp[1024];
+
+      itk::Index<3> seedPosition;
+
+      ModuleType  module;
+      module.SetPluginInfo( info );
+      module.SetUpdateMessage("Computing Geodesic Active Contour Module...");
+      module.SetDistanceFromSeeds( distanceFromSeeds );
+      module.SetSigma( gaussianSigma );
+      module.SetLowestBasinValue( lowestBasinValue ); 
+      module.SetLowestBorderValue( lowestBorderValue );
+      module.SetCurvatureScaling( curvatureScaling );
+      module.SetPropagationScaling( propagationScaling );
+      module.SetAdvectionScaling( advectionScaling );
+      module.SetMaximumRMSError( maximumRMSError );
+      module.SetMaximumIterations( maximumNumberOfIterations );
+      for(unsigned int i=0; i< numberOfSeeds; i++)
+        {
+        VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, i, seedPosition );
+        module.AddSeed( seedPosition );
+        }
+      // Execute the filter
+      module.ProcessData( pds  );
+      sprintf(tmp,"Total number of iterations = %d \n Final RMS error = %f",
+                         module.GetElapsedIterations(),
+                         module.GetRMSChange());
+      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
+    }
+  };
+
 static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
 {
 
   vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
 
-  const float distanceFromSeeds     = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ));
-  const float gaussianSigma         = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ));
-  const float lowestBasinValue      = atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ));
-  const float lowestBorderValue     = atof( info->GetGUIProperty(info, 3, VVP_GUI_VALUE ));
-  const float curvatureScaling      = atof( info->GetGUIProperty(info, 4, VVP_GUI_VALUE ));
-  const float propagationScaling    = atof( info->GetGUIProperty(info, 5, VVP_GUI_VALUE ));
-  const float advectionScaling      = atof( info->GetGUIProperty(info, 6, VVP_GUI_VALUE ));
-  const float maximumRMSError       = atof( info->GetGUIProperty(info, 7, VVP_GUI_VALUE ));
+  if( info->InputVolumeNumberOfComponents != 1 )
+    {
+    info->SetProperty( info, VVP_ERROR, "This filter requires a single-component data set as input" ); 
+    return -1;
+    }
 
-  const unsigned int maximumNumberOfIterations = atoi( info->GetGUIProperty(info, 8, VVP_GUI_VALUE ));
-
-  const unsigned int numberOfSeeds = info->NumberOfMarkers;
-  if( numberOfSeeds < 1 )
+  if( info->NumberOfMarkers < 1 )
     {
     info->SetProperty( info, VVP_ERROR, "Please select points using the 3D Markers in the Annotation menu" ); 
     return -1;
     }
 
-  char tmp[1024];
-
-  itk::Index<3> seedPosition;
-
   try 
   {
   switch( info->InputVolumeScalarType )
     {
+    case VTK_CHAR:
+      {
+      GeodesicActiveContourModuleRunner<signed char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
     case VTK_UNSIGNED_CHAR:
       {
-      typedef  unsigned char                              PixelType;
-      typedef VolView::PlugIn::GeodesicActiveContourModule< 
-                                            PixelType >   ModuleType;
-      ModuleType  module;
-      module.SetPluginInfo( info );
-      module.SetUpdateMessage("Computing Geodesic Active Contour Module...");
-      module.SetDistanceFromSeeds( distanceFromSeeds );
-      module.SetSigma( gaussianSigma );
-      module.SetLowestBasinValue( lowestBasinValue ); 
-      module.SetLowestBorderValue( lowestBorderValue );
-      module.SetCurvatureScaling( curvatureScaling );
-      module.SetPropagationScaling( propagationScaling );
-      module.SetAdvectionScaling( advectionScaling );
-      module.SetMaximumRMSError( maximumRMSError );
-      module.SetMaximumIterations( maximumNumberOfIterations );
-      for(unsigned int i=0; i< numberOfSeeds; i++)
-        {
-        VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, i, seedPosition );
-        module.AddSeed( seedPosition );
-        }
-      // Execute the filter
-      module.ProcessData( pds  );
-      sprintf(tmp,"Total number of iterations = %d \n Final RMS error = %f",
-                         module.GetElapsedIterations(),
-                         module.GetRMSChange());
-      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
+      GeodesicActiveContourModuleRunner<unsigned char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_SHORT:
+      {
+      GeodesicActiveContourModuleRunner<signed short> runner;
+      runner.Execute( info, pds );
       break; 
       }
     case VTK_UNSIGNED_SHORT:
       {
-      typedef  unsigned short                             PixelType;
-      typedef VolView::PlugIn::GeodesicActiveContourModule< 
-                                            PixelType >   ModuleType;
-      ModuleType  module;
-      module.SetPluginInfo( info );
-      module.SetDistanceFromSeeds( distanceFromSeeds );
-      module.SetSigma( gaussianSigma );
-      module.SetUpdateMessage("Computing Geodesic Active Contour Module...");
-      module.SetLowestBasinValue( lowestBasinValue ); 
-      module.SetLowestBorderValue( lowestBorderValue );
-      module.SetCurvatureScaling( curvatureScaling );
-      module.SetPropagationScaling( propagationScaling );
-      module.SetAdvectionScaling( advectionScaling );
-      module.SetMaximumRMSError( maximumRMSError );
-      module.SetMaximumIterations( maximumNumberOfIterations );
-      for(unsigned int i=0; i< numberOfSeeds; i++)
-        {
-        VolView::PlugIn::FilterModuleBase::Convert3DMarkerToIndex( info, i, seedPosition );
-        module.AddSeed( seedPosition );
-        }
-      // Execute the filter
-      module.ProcessData( pds  );
-      sprintf(tmp,"Total number of iterations = %d \n Final RMS error = %f",
-                         module.GetElapsedIterations(),
-                         module.GetRMSChange());
-      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
+      GeodesicActiveContourModuleRunner<unsigned short> runner;
+      runner.Execute( info, pds );
       break; 
-      } 
+      }
+    case VTK_INT:
+      {
+      GeodesicActiveContourModuleRunner<signed int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_INT:
+      {
+      GeodesicActiveContourModuleRunner<unsigned int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_LONG:
+      {
+      GeodesicActiveContourModuleRunner<signed long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_LONG:
+      {
+      GeodesicActiveContourModuleRunner<unsigned long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_FLOAT:
+      {
+      GeodesicActiveContourModuleRunner<float> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_DOUBLE:
+      {
+      GeodesicActiveContourModuleRunner<double> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
     }
+
   }
   catch( itk::ExceptionObject & except )
   {
