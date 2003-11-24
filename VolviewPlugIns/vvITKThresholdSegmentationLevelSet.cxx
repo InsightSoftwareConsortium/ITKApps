f2a393d785bfc61e4458564ddfb0a30f59e30f16
Luis Ibanez <luis.ibanez@kitware.com>
2003-11-24 14:12:27 -0500

    Plugin for the level set method: ThresholdSegmentation.

---
+++ VolviewPlugIns/vvITKThresholdSegmentationLevelSet.cxx
@@ -0,0 +1,217 @@
+/* This plugin adapts the ThresholdSegmentationLevelSet filter.
+   It does not perform any preprocessing. The user should provide
+   the speed image and the initial level set as inputs. */
+
+#include "vvITKThresholdSegmentationLevelSet.h"
+
+
+
+template <class InputPixelType>
+class ThresholdSegmentationLevelSeteRunner
+  {
+  public:
+    typedef itk::Image< InputPixelType, 3 > InputImageType;
+    typedef InputImageType                  InputFeatureImageType;
+    
+    typedef VolView::PlugIn::ThresholdSegmentationLevelSet< 
+                                                  InputImageType,
+                                                  InputFeatureImageType >   ModuleType;
+    
+  public:
+    ThresholdSegmentationLevelSeteRunner() {}
+    void Execute( vtkVVPluginInfo *info, vtkVVProcessDataStruct *pds )
+    {
+      char tmp[1024];
+
+      ModuleType  module;
+      module.SetPluginInfo( info );
+      module.SetUpdateMessage("Computing Segmentation Threshold Level Set...");
+      module.ProcessData( pds  );
+      sprintf(tmp,"Total number of iterations = %d \n Final RMS error = %g",
+                         module.GetFilter()->GetElapsedIterations(),
+                         module.GetFilter()->GetRMSChange());
+      info->SetProperty( info, VVP_REPORT_TEXT, tmp );
+
+    }
+  };
+
+
+
+
+static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
+{
+
+  vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
+
+  if( info->InputVolumeNumberOfComponents != 1 )
+    {
+    info->SetProperty( info, VVP_ERROR, "This filter requires a single-component data set as input" ); 
+    return -1;
+    }
+
+  try 
+  {
+  switch( info->InputVolumeScalarType )
+    {
+    case VTK_CHAR:
+      {
+      ThresholdSegmentationLevelSeteRunner<signed char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_CHAR:
+      {
+      ThresholdSegmentationLevelSeteRunner<unsigned char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_SHORT:
+      {
+      ThresholdSegmentationLevelSeteRunner<signed short> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_SHORT:
+      {
+      ThresholdSegmentationLevelSeteRunner<unsigned short> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_INT:
+      {
+      ThresholdSegmentationLevelSeteRunner<signed int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_INT:
+      {
+      ThresholdSegmentationLevelSeteRunner<unsigned int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_LONG:
+      {
+      ThresholdSegmentationLevelSeteRunner<signed long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_LONG:
+      {
+      ThresholdSegmentationLevelSeteRunner<unsigned long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_FLOAT:
+      {
+      ThresholdSegmentationLevelSeteRunner<float> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_DOUBLE:
+      {
+      ThresholdSegmentationLevelSeteRunner<double> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    }
+  }
+  catch( itk::ExceptionObject & except )
+  {
+    info->SetProperty( info, VVP_ERROR, except.what() ); 
+    return -1;
+  }
+
+  info->UpdateProgress( info, 1.0, "Segmentation Threshold LevelSet Done !");
+
+  return 0;
+}
+
+
+static int UpdateGUI(void *inf)
+{
+  vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
+
+  info->SetGUIProperty(info, 0, VVP_GUI_LABEL, "Lower Threshold.");
+  info->SetGUIProperty(info, 0, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 0, VVP_GUI_DEFAULT, VolView::PlugIn::FilterModuleBase::GetInputVolumeScalarMinimum( info ) );
+  info->SetGUIProperty(info, 0, VVP_GUI_HELP, "Lower threshold for binarizing the feature image and induce attraction to the edges");
+  info->SetGUIProperty(info, 0, VVP_GUI_HINTS, VolView::PlugIn::FilterModuleBase::GetInputVolumeScalarRange( info ) );
+
+  info->SetGUIProperty(info, 1, VVP_GUI_LABEL, "Upper Threshold.");
+  info->SetGUIProperty(info, 1, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 1, VVP_GUI_DEFAULT, VolView::PlugIn::FilterModuleBase::GetInputVolumeScalarMinimum( info ) );
+  info->SetGUIProperty(info, 1, VVP_GUI_HELP, "Upper threshold for binarizing the feature image and induce attraction to the edges");
+  info->SetGUIProperty(info, 1, VVP_GUI_HINTS, VolView::PlugIn::FilterModuleBase::GetInputVolumeScalarRange( info ) );
+
+  info->SetGUIProperty(info, 2, VVP_GUI_LABEL, "Curvature scaling.");
+  info->SetGUIProperty(info, 2, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 2, VVP_GUI_DEFAULT, "1.0");
+  info->SetGUIProperty(info, 2, VVP_GUI_HELP, "Scaling factor for the curvature contribution. Larger values will result in smoother contours");
+  info->SetGUIProperty(info, 2, VVP_GUI_HINTS , "0.1 10.0 0.1");
+
+  info->SetGUIProperty(info, 3, VVP_GUI_LABEL, "Propagation scaling.");
+  info->SetGUIProperty(info, 3, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 3, VVP_GUI_DEFAULT, "1.0");
+  info->SetGUIProperty(info, 3, VVP_GUI_HELP, "Scaling factor for the inflation factor. Larger factors will result in rapid expansion with irregular borders");
+  info->SetGUIProperty(info, 3, VVP_GUI_HINTS , "0.1 10.0 0.1");
+
+  info->SetGUIProperty(info, 4, VVP_GUI_LABEL, "Advection scaling.");
+  info->SetGUIProperty(info, 4, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 4, VVP_GUI_DEFAULT, "1.0");
+  info->SetGUIProperty(info, 4, VVP_GUI_HELP, "Scaling factor for the advection factor. This factor generates a force that is proportional to the slant of the level set at a point.");
+  info->SetGUIProperty(info, 4, VVP_GUI_HINTS , "0.1 10.0 0.1");
+
+  info->SetGUIProperty(info, 5, VVP_GUI_LABEL, "Maximum RMS Error.");
+  info->SetGUIProperty(info, 5, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 5, VVP_GUI_DEFAULT, "0.06");
+  info->SetGUIProperty(info, 5, VVP_GUI_HELP, "Threshold of the RMS change between one iteration and the previous one. This is a convergence criteria, the process will stop when the RMS change is lower than the value set here");
+  info->SetGUIProperty(info, 5, VVP_GUI_HINTS , "0.01 0.5 0.01");
+
+  info->SetGUIProperty(info, 6, VVP_GUI_LABEL, "Maximum iterations.");
+  info->SetGUIProperty(info, 6, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 6, VVP_GUI_DEFAULT, "100.0");
+  info->SetGUIProperty(info, 6, VVP_GUI_HELP, "The maximum number of iteration to apply the time step in the partial differental equation.");
+  info->SetGUIProperty(info, 6, VVP_GUI_HINTS , "1.0 500.0 1.0");
+
+  info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP, "0");
+  
+  info->OutputVolumeScalarType = VTK_UNSIGNED_CHAR;
+  info->OutputVolumeNumberOfComponents = 1;
+
+  memcpy(info->OutputVolumeDimensions,info->InputVolumeDimensions,
+         3*sizeof(int));
+  memcpy(info->OutputVolumeSpacing,info->InputVolumeSpacing,
+         3*sizeof(float));
+  memcpy(info->OutputVolumeOrigin,info->InputVolumeOrigin,
+         3*sizeof(float));
+
+  return 1;
+}
+
+
+extern "C" {
+  
+void VV_PLUGIN_EXPORT vvITKThresholdSegmentationLevelSetInit(vtkVVPluginInfo *info)
+{
+  vvPluginVersionCheck();
+
+  // setup information that never changes
+  info->ProcessData = ProcessData;
+  info->UpdateGUI   = UpdateGUI;
+  info->SetProperty(info, VVP_NAME, "Segmentation Threshold Level Set (ITK)");
+  info->SetProperty(info, VVP_GROUP, "Segmentation - Level Sets");
+  info->SetProperty(info, VVP_TERSE_DOCUMENTATION,
+                                    "Segmentation Threshold LevelSet");
+  info->SetProperty(info, VVP_FULL_DOCUMENTATION,
+    "This module applies the Segmentation Threshold Level Set method for segmenting a volume. This method evolves a level set under the attraction of the edges of a binary image. This binary image is computed by thresholding the feature image provided here as the second input. No preprocessing is performed here. The user must provide as inputs an initial level set and the feature image which will be used to compute speeds. The current image being visualized will be taken as the initial level set. The speed image required  as a second input can be specified by providinng a file name in the GUI.");
+
+  info->SetProperty(info, VVP_SUPPORTS_IN_PLACE_PROCESSING, "0");
+  info->SetProperty(info, VVP_SUPPORTS_PROCESSING_PIECES,   "0");
+  info->SetProperty(info, VVP_NUMBER_OF_GUI_ITEMS,          "7");
+  info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP,           "0");
+  info->SetProperty(info, VVP_PER_VOXEL_MEMORY_REQUIRED,   "16");
+  info->SetProperty(info, VVP_REQUIRES_SECOND_INPUT,        "1");
+
+}
+
+}
---
+++ VolviewPlugIns/vvITKThresholdSegmentationLevelSet.h
@@ -0,0 +1,147 @@
+/** This module implements the limited preprocessing for 
+    the ThresholdSegmentationLevelSets filter. The module expects
+    two inputs. First, a binary mask, second a speed image.
+    the binary mask is passed through the ReinitializeImageFilter
+    in order to produce a level set. The speed image is casted
+    and renormalized between 0.0 and 1.0, then connected as
+    feature image to the geodesic active contour filter.  */
+    
+
+#ifndef _vvITKThresholdSegmentationLevelSet_h
+#define _vvITKThresholdSegmentationLevelSet_h
+
+#include "vvITKFilterModuleTwoInputs.h"
+#include "itkThresholdSegmentationLevelSetImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkSmoothingRecursiveGaussianImageFilter.h"
+
+
+namespace VolView
+{
+
+namespace PlugIn
+{
+
+
+template< class TInputLevelSetImageType, class TFeatureImageType>
+class ThresholdSegmentationLevelSet : public 
+                   FilterModuleTwoInputs<
+                          itk::ThresholdSegmentationLevelSetImageFilter<
+                                            TInputLevelSetImageType,
+                                            TFeatureImageType >,
+                              TInputLevelSetImageType,
+                              TFeatureImageType > {
+
+public:
+
+  typedef TInputLevelSetImageType    InputLevelSetImageType;
+  typedef TFeatureImageType          InputFeatureImageType;
+
+  typedef  FilterModuleTwoInputs<
+                      itk::ThresholdSegmentationLevelSetImageFilter<
+                                        TInputLevelSetImageType,
+                                        TFeatureImageType >,
+                      InputLevelSetImageType,
+                      InputFeatureImageType > Superclass;
+
+  typedef itk::ThresholdSegmentationLevelSetImageFilter<
+                          TInputLevelSetImageType,
+                          TFeatureImageType
+                                  > ThresholdSegmentationLevelSetFilterType;
+ 
+  typedef typename ThresholdSegmentationLevelSetFilterType::OutputImageType  OutputImageType;
+  typedef typename OutputImageType::PixelType                                OutputPixelType;
+
+public:
+
+  /**  Constructor */
+  ThresholdSegmentationLevelSet() 
+    {
+    }
+
+
+
+  /**  Destructor */
+  virtual ~ThresholdSegmentationLevelSet() 
+    {
+    }
+
+
+  /**  ProcessData performs the actual filtering on the data.
+       In this class, this method only initialize the import
+       filter for the second input, then it lets the ProcessData
+       method of the base class perform the rest of the operations. */
+  void 
+  ProcessData( const vtkVVProcessDataStruct * pds )
+  {
+    // Let superclass perform initial connections
+    this->Superclass::ProcessData( pds );
+
+    ThresholdSegmentationLevelSetFilterType * filter = this->GetFilter();
+
+    vtkVVPluginInfo *info = this->GetPluginInfo();
+
+    const float upperThreshold        = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ));
+    const float lowerThreshold        = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ));
+    const float curvatureScaling      = atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ));
+    const float propagationScaling    = atof( info->GetGUIProperty(info, 3, VVP_GUI_VALUE ));
+    const float advectionScaling      = atof( info->GetGUIProperty(info, 4, VVP_GUI_VALUE ));
+    const float maximumRMSError       = atof( info->GetGUIProperty(info, 5, VVP_GUI_VALUE ));
+
+    const unsigned int maximumNumberOfIterations = atoi( info->GetGUIProperty(info, 6, VVP_GUI_VALUE ));
+
+    filter->SetLowerThreshold( lowerThreshold );
+    filter->SetUpperThreshold( upperThreshold );
+    filter->SetCurvatureScaling( curvatureScaling );
+    filter->SetPropagationScaling( propagationScaling );
+    filter->SetAdvectionScaling( advectionScaling );
+    filter->SetMaximumRMSError( maximumRMSError );
+    filter->SetMaximumIterations( maximumNumberOfIterations );
+
+    filter->SetInput(        this->GetInput1() );
+    filter->SetFeatureImage( this->GetInput2() );
+
+    // Execute the filter
+    try
+      {
+      filter->Update();
+      filter->ReleaseDataFlagOn();
+      }
+    catch( itk::ProcessAborted & )
+      {
+      return;
+      }
+
+    // Copy the data (with casting) to the output buffer provided by the PlugIn API
+    OutputImageType::ConstPointer outputImage =
+                                     filter->GetOutput();
+
+    typedef itk::ImageRegionConstIterator< OutputImageType >  OutputIteratorType;
+
+    OutputIteratorType ot( outputImage, outputImage->GetBufferedRegion() );
+
+    OutputPixelType * outData = (OutputPixelType *)(pds->outData);
+
+    ot.GoToBegin(); 
+    while( !ot.IsAtEnd() )
+      {
+      *outData = ot.Get();
+      ++ot;
+      ++outData;
+      }
+
+  } // end of ProcessData
+
+
+
+private:
+
+
+};
+
+
+} // end namespace PlugIn
+
+} // end namespace VolView
+
+#endif
