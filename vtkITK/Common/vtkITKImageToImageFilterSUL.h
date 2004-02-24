200a11f2e8dcb6d0e0836f28fc18e84be479994e
Jim Miller <millerjv@crd.ge.com>
2004-02-24 16:25:30 -0500

    FIX: cast type was incorrect

--- vtkITK/Common/vtkITKImageToImageFilterSUL.h
+++ vtkITK/Common/vtkITKImageToImageFilterSUL.h
@@ -57,7 +57,7 @@ protected:
     // Set up the filter pipeline
     m_Filter->SetInput ( this->itkImporter->GetOutput() );
     this->itkExporter->SetInput ( m_Filter->GetOutput() );
-    this->vtkCast->SetOutputScalarTypeToUnsignedShort();
+    this->vtkCast->SetOutputScalarTypeToShort();
   };
 
   ~vtkITKImageToImageFilterSUL()
