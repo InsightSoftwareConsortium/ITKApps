2cafed9a203de8b96b19a7ec8d11ed0a790de17a
Luis Ibanez <luis.ibanez@kitware.com>
2004-04-06 17:53:22 -0400

    Pluging for Surface smoothing using 4th Order level sets.

---
+++ VolviewPlugIns/vvITKIsotropicFourthOrderLevelSet.cxx
@@ -0,0 +1,218 @@
+/* perform smoothing using an anisotropic diffusion filter */
+
+#include "vvITKFilterModuleWithRescaling.h"
+
+#include "itkIsotropicFourthOrderLevelSetImageFilter.h"
+
+
+
+template <class TInputPixelType>
+class IsotropicFourthOrderLevelSetRunner
+  {
+  public:
+      typedef  TInputPixelType                           InputPixelType;
+      typedef  itk::Image< InputPixelType, 3 >           InputImageType; 
+      typedef   float                                    InternalPixelType;
+      typedef   itk::Image< InternalPixelType,3 >        InternalImageType; 
+
+      typedef   itk::IsotropicFourthOrderLevelSetImageFilter< 
+                                    InputImageType,  
+                                    InternalImageType >  FilterType;
+     
+      typedef  unsigned char                             OutputPixelType;
+
+      typedef  VolView::PlugIn::FilterModuleWithRescaling< 
+                                                FilterType,
+                                                OutputPixelType > ModuleType;
+
+  public:
+    IsotropicFourthOrderLevelSetRunner() {}
+    void Execute( vtkVVPluginInfo *info, vtkVVProcessDataStruct *pds )
+    {
+      const unsigned int    maxNumberOfIterations = atoi( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ) );
+      const float           maximumRMSError       = atof( info->GetGUIProperty(info, 1, VVP_GUI_VALUE ) );
+      const InputPixelType  isoSurfaceValue       = 
+                       static_cast<InputPixelType>( atof( info->GetGUIProperty(info, 2, VVP_GUI_VALUE ) ) );
+
+      ModuleType  module;
+      module.SetPluginInfo( info );
+      module.SetUpdateMessage("Smoothing Iso-Surface...");
+      // Set the parameters on it
+      module.GetFilter()->SetNumberOfIterations(     maxNumberOfIterations );
+      module.GetFilter()->SetMaximumRMSError(        maximumRMSError       );
+      module.GetFilter()->SetIsoSurfaceValue(        isoSurfaceValue       );
+      module.SetOutputMinimum(  0  );
+      module.SetOutputMaximum( 255 );
+      // Execute the filter
+      module.ProcessData( pds  );
+    }
+  };
+
+
+static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
+{
+
+  vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
+
+  // make sure there is only one component of input data
+  if (info->InputVolumeNumberOfComponents != 1)
+    {
+    info->SetProperty( info, VVP_ERROR, 
+                       "The IsotropicFourthOrderLevelSet filter only works with single component data" ); 
+    return -1;
+    }
+  
+
+  try 
+  {
+  switch( info->InputVolumeScalarType )
+    {
+    case VTK_CHAR:
+      {
+      IsotropicFourthOrderLevelSetRunner<signed char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_CHAR:
+      {
+      IsotropicFourthOrderLevelSetRunner<unsigned char> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_SHORT:
+      {
+      IsotropicFourthOrderLevelSetRunner<signed short> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_SHORT:
+      {
+      IsotropicFourthOrderLevelSetRunner<unsigned short> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_INT:
+      {
+      IsotropicFourthOrderLevelSetRunner<signed int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_INT:
+      {
+      IsotropicFourthOrderLevelSetRunner<unsigned int> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_LONG:
+      {
+      IsotropicFourthOrderLevelSetRunner<signed long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_UNSIGNED_LONG:
+      {
+      IsotropicFourthOrderLevelSetRunner<unsigned long> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_FLOAT:
+      {
+      IsotropicFourthOrderLevelSetRunner<float> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    case VTK_DOUBLE:
+      {
+      IsotropicFourthOrderLevelSetRunner<double> runner;
+      runner.Execute( info, pds );
+      break; 
+      }
+    default:
+      info->SetProperty( info, VVP_ERROR, "Pixel Type Unknown for the IsotropicFourthOrderLevelSet filter" ); 
+      return -1;
+      break;
+    }
+  }
+  catch( itk::ExceptionObject & except )
+  {
+    info->SetProperty( info, VVP_ERROR, except.what() ); 
+    return -1;
+  }
+  return 0;
+}
+
+
+static int UpdateGUI(void *inf)
+{
+  vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
+
+  info->SetGUIProperty(info, 0, VVP_GUI_LABEL, "Number of Iterations ");
+  info->SetGUIProperty(info, 0, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 0, VVP_GUI_DEFAULT, "5");
+  info->SetGUIProperty(info, 0, VVP_GUI_HELP, "Number of times that the diffusion approximation will be computed. The more iterations, the stronger the smoothing");
+  info->SetGUIProperty(info, 0, VVP_GUI_HINTS , "1 100 1");
+
+  info->SetGUIProperty(info, 1, VVP_GUI_LABEL, "Maximum RMS Error");
+  info->SetGUIProperty(info, 1, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 1, VVP_GUI_DEFAULT, "0.05");
+  info->SetGUIProperty(info, 1, VVP_GUI_HELP, "Maximum RMS error allows. This value defines the convergence criterion for the smoothing.");
+  info->SetGUIProperty(info, 1, VVP_GUI_HINTS , "0.001 0.1 0.001");
+
+  info->SetGUIProperty(info, 2, VVP_GUI_LABEL, "Iso-Surface Value");
+  info->SetGUIProperty(info, 2, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 2, VVP_GUI_DEFAULT, VolView::PlugIn::FilterModuleBase::GetInputVolumeScalarMidValue( info ) ); 
+  info->SetGUIProperty(info, 2, VVP_GUI_HELP, "The Iso-Surface associated with this value is the one that will be smoothed by the repeated application of this filter. Note that the output image will be rescaled using 8 bits, therefore the output iso-surface must be searched as the value 128 of the image computed as result of this filter.");
+  info->SetGUIProperty(info, 2, VVP_GUI_HINTS, VolView::PlugIn::FilterModuleBase::GetInputVolumeScalarRange( info ) );
+
+
+  const char * stringValue = info->GetGUIProperty(info, 0, VVP_GUI_VALUE );
+  if( !stringValue )
+    {
+    info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP, "0");
+    }
+  else
+    {
+    info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP, stringValue);
+    }
+  
+
+  info->OutputVolumeScalarType = VTK_UNSIGNED_CHAR;
+  info->OutputVolumeNumberOfComponents = 1;
+
+  info->OutputVolumeDimensions[0] = info->InputVolumeDimensions[0]; 
+  info->OutputVolumeDimensions[1] = info->InputVolumeDimensions[1];
+  info->OutputVolumeDimensions[2] = info->InputVolumeDimensions[2];
+
+  memcpy(info->OutputVolumeSpacing,info->InputVolumeSpacing,
+         3*sizeof(float));
+  memcpy(info->OutputVolumeOrigin,info->InputVolumeOrigin,
+         3*sizeof(float));
+
+  return 1;
+}
+
+extern "C" {
+  
+void VV_PLUGIN_EXPORT vvITKIsotropicFourthOrderLevelSetInit(vtkVVPluginInfo *info)
+{
+  vvPluginVersionCheck();
+
+  // setup information that never changes
+  info->ProcessData = ProcessData;
+  info->UpdateGUI = UpdateGUI;
+  info->SetProperty(info, VVP_NAME, "Fourth-Order Level Set (ITK)");
+  info->SetProperty(info, VVP_GROUP, "Surface Generation");
+  info->SetProperty(info, VVP_TERSE_DOCUMENTATION,
+                                  "Reduction of aliasing effects");
+  info->SetProperty(info, VVP_FULL_DOCUMENTATION,
+    "This filter applies a level set evolution over a binary image in order to produce a smoother contour that is suitable for extracting iso-surfaces. The resulting contour is encoded as the zero-set of the output level set. The zero set will be rescaled as the mid-value of the intensity range corresponding to the pixel type used. This filter processes the whole image in one piece, and does not change the dimensions, or spacing of the volume. The pixel type however, is converted to unsigned 8 bits since it is enough for representing the implicit smoothed surface.");
+
+  info->SetProperty(info, VVP_SUPPORTS_IN_PLACE_PROCESSING, "0");
+  info->SetProperty(info, VVP_SUPPORTS_PROCESSING_PIECES,   "0");
+  info->SetProperty(info, VVP_NUMBER_OF_GUI_ITEMS,          "3");
+  info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP,           "0");
+  info->SetProperty(info, VVP_PER_VOXEL_MEMORY_REQUIRED,    "8"); 
+
+}
+
+}
