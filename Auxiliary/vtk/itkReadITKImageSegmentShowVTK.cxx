e9308b33f9db1816f0b6a290774c30b175889530
Luis Ibanez <luis.ibanez@kitware.com>
2004-08-28 18:39:21 -0400

    ENH: Example illustrating how to visualize the edges of a segmentation      overlaid on top of the original image.

---
+++ Auxiliary/vtk/itkReadITKImageSegmentShowVTK.cxx
@@ -0,0 +1,212 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkReadITKImageSegmentShowVTK.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkCommand.h"
+#include "itkImage.h"
+#include "itkVTKImageExport.h"
+#include "itkVTKImageImport.h"
+#include "itkConfidenceConnectedImageFilter.h"
+#include "itkCastImageFilter.h"
+#include "itkRGBPixel.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "vtkImageImport.h"
+#include "vtkImageExport.h"
+#include "vtkImageActor.h"
+#include "vtkInteractorStyleImage.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkActor.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkContourFilter.h"
+#include "vtkImageData.h"
+#include "vtkDataSet.h"
+#include "vtkProperty.h"
+
+
+/**
+ * This function will connect the given itk::VTKImageExport filter to
+ * the given vtkImageImport filter.
+ */
+template <typename ITK_Exporter, typename VTK_Importer>
+void ConnectPipelines(ITK_Exporter exporter, VTK_Importer* importer)
+{
+  importer->SetUpdateInformationCallback(exporter->GetUpdateInformationCallback());
+  importer->SetPipelineModifiedCallback(exporter->GetPipelineModifiedCallback());
+  importer->SetWholeExtentCallback(exporter->GetWholeExtentCallback());
+  importer->SetSpacingCallback(exporter->GetSpacingCallback());
+  importer->SetOriginCallback(exporter->GetOriginCallback());
+  importer->SetScalarTypeCallback(exporter->GetScalarTypeCallback());
+  importer->SetNumberOfComponentsCallback(exporter->GetNumberOfComponentsCallback());
+  importer->SetPropagateUpdateExtentCallback(exporter->GetPropagateUpdateExtentCallback());
+  importer->SetUpdateDataCallback(exporter->GetUpdateDataCallback());
+  importer->SetDataExtentCallback(exporter->GetDataExtentCallback());
+  importer->SetBufferPointerCallback(exporter->GetBufferPointerCallback());
+  importer->SetCallbackUserData(exporter->GetCallbackUserData());
+}
+
+/**
+ * This function will connect the given vtkImageExport filter to
+ * the given itk::VTKImageImport filter.
+ */
+template <typename VTK_Exporter, typename ITK_Importer>
+void ConnectPipelines(VTK_Exporter* exporter, ITK_Importer importer)
+{
+  importer->SetUpdateInformationCallback(exporter->GetUpdateInformationCallback());
+  importer->SetPipelineModifiedCallback(exporter->GetPipelineModifiedCallback());
+  importer->SetWholeExtentCallback(exporter->GetWholeExtentCallback());
+  importer->SetSpacingCallback(exporter->GetSpacingCallback());
+  importer->SetOriginCallback(exporter->GetOriginCallback());
+  importer->SetScalarTypeCallback(exporter->GetScalarTypeCallback());
+  importer->SetNumberOfComponentsCallback(exporter->GetNumberOfComponentsCallback());
+  importer->SetPropagateUpdateExtentCallback(exporter->GetPropagateUpdateExtentCallback());
+  importer->SetUpdateDataCallback(exporter->GetUpdateDataCallback());
+  importer->SetDataExtentCallback(exporter->GetDataExtentCallback());
+  importer->SetBufferPointerCallback(exporter->GetBufferPointerCallback());
+  importer->SetCallbackUserData(exporter->GetCallbackUserData());
+}
+
+
+/**
+ * This program implements an example connection between ITK and VTK
+ * pipelines.  The combined pipeline flows as follows:
+ *
+ * itkImageFileReader ==> itkVTKImageExport ==>
+ *    vtkImageImport ==> vtkImageActor
+ *
+ * The resulting vtkImageActor is displayed in a vtkRenderWindow.
+ * Whenever the VTK pipeline executes, information is propagated
+ * through the ITK pipeline.  If the ITK pipeline is out of date, it
+ * will re-execute and cause the VTK pipeline to update properly as
+ * well.
+ */
+int main(int argc, char * argv [] )
+{  
+
+  // Load a color image using ITK and display it with VTK
+
+  if( argc < 2 )
+    {
+    std::cerr << "Missing parameters" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " inputImageFilename " << std::endl;
+    return 1;
+    }
+  
+  try
+    {
+    typedef unsigned char PixelType;
+    typedef itk::Image< PixelType, 2 > ImageType;
+    
+    typedef itk::ImageFileReader< ImageType > ReaderType;
+
+    ReaderType::Pointer reader  = ReaderType::New();
+    reader->SetFileName( argv[1] );
+    reader->Update();
+
+    typedef itk::ConfidenceConnectedImageFilter<ImageType,ImageType> SegmentationFilterType;
+
+    SegmentationFilterType::Pointer filter = SegmentationFilterType::New();
+
+    filter->SetInput( reader->GetOutput() );
+
+    filter->SetNumberOfIterations(2);
+    filter->SetReplaceValue(255);
+    filter->SetMultiplier(2.5);
+
+    ImageType::IndexType index;
+    index[0] = 100;
+    index[1] = 100;
+
+    filter->SetSeed( index );
+      
+    
+    typedef itk::VTKImageExport< ImageType > ExportFilterType;
+    ExportFilterType::Pointer itkExporter1 = ExportFilterType::New();
+    ExportFilterType::Pointer itkExporter2 = ExportFilterType::New();
+
+    itkExporter1->SetInput( reader->GetOutput() );
+    itkExporter2->SetInput( filter->GetOutput() );
+
+    // Create the vtkImageImport and connect it to the
+    // itk::VTKImageExport instance.
+    vtkImageImport* vtkImporter1 = vtkImageImport::New();  
+    ConnectPipelines(itkExporter1, vtkImporter1);
+    
+    vtkImageImport* vtkImporter2 = vtkImageImport::New();  
+    ConnectPipelines(itkExporter2, vtkImporter2);
+    
+
+    //------------------------------------------------------------------------
+    // VTK pipeline.
+    //------------------------------------------------------------------------
+    
+    // Create a vtkImageActor to help render the image.  Connect it to
+    // the vtkImporter instance.
+    vtkImageActor* actor = vtkImageActor::New();
+    actor->SetInput(vtkImporter1->GetOutput());
+    
+    vtkInteractorStyleImage * interactorStyle = vtkInteractorStyleImage::New();
+
+
+    // Create a renderer, render window, and render window interactor to
+    // display the results.
+    vtkRenderer* renderer = vtkRenderer::New();
+    vtkRenderWindow* renWin = vtkRenderWindow::New();
+    vtkRenderWindowInteractor* iren = vtkRenderWindowInteractor::New();
+    
+    renWin->SetSize(500, 500);
+    renWin->AddRenderer(renderer);
+    iren->SetRenderWindow(renWin);
+    iren->SetInteractorStyle( interactorStyle );
+    
+    // Add the vtkImageActor to the renderer for display.
+    renderer->AddActor(actor);
+    renderer->SetBackground(0.4392, 0.5020, 0.5647);
+
+
+    // Draw contours around the segmented regions
+    vtkContourFilter * contour = vtkContourFilter::New();
+    contour->SetInput( vtkImporter2->GetOutput() );
+    contour->SetValue(0, 128); // edges of a binary image with values 0,255
+
+
+    vtkPolyDataMapper * polyMapper = vtkPolyDataMapper::New();
+    vtkActor          * polyActor  = vtkActor::New();
+
+    polyActor->SetMapper( polyMapper );
+    polyMapper->SetInput( contour->GetOutput() );
+    polyActor->GetProperty()->SetColor(1,0,0);
+    
+    renderer->AddActor( polyActor );
+
+    // Bring up the render window and begin interaction.
+    renWin->Render();
+    iren->Start();
+
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cerr << "Exception catched !! " << e << std::endl;
+    }
+
+
+  
+  return 0;
+}
