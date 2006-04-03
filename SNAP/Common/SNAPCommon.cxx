1960579ddbbefb5e0bfedb66b776e9120d3ffe61
Paul Yushkevich <pauly@cognitica.com>
2006-04-02 22:50:17 -0400

    ENH: SNAP versioning for release 1.4

--- SNAP/Common/SNAPCommon.cxx
+++ SNAP/Common/SNAPCommon.cxx
@@ -21,13 +21,13 @@ const char SNAPVersionShort[] = "$Name:  $";
 const char SNAPVersion[] = "@(#) Id";
 
 // Non-cvs version
-const char SNAPSoftVersion[] = "ITK-SNAP 1.4 RC2";
+const char SNAPSoftVersion[] = "ITK-SNAP 1.4";
 
 // A string that appears in the user interface
-const char SNAPUISoftVersion[] = "Version 1.4 RC2\n Release Candidate 2\nFeb 01, 2006";
+const char SNAPUISoftVersion[] = "Version 1.4\n Apr 01, 2006";
 
 // Release date of the current version
-const char SNAPCurrentVersionReleaseDate[] = "20060201";
+const char SNAPCurrentVersionReleaseDate[] = "20060401";
 
 // Release date of the latest version whose user preference files are
 // incompatible with the current version and will be erased
